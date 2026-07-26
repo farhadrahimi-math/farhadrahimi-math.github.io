1867a514990ab3dac703193607457e859bae2756
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-26 13:10:42 +0330

    Create auth-guard.js

---
+++ auth-guard.js
@@ -0,0 +1,44 @@
+const supabaseUrl = "https://ypjmkigvghybkwyxndcz.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwam1raWd2Z2h5Ymt3eXhuZGN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMTc1NTgsImV4cCI6MjA5OTY5MzU1OH0.lJ5RddKmDdPfLecBsqL9XMGejL9Owbv1ZH2PXSqqdv4";
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+async function authGuard(requiredGrade = null){
+
+  const { data:{session} } = await supabaseClient.auth.getSession();
+
+  if(!session){
+    window.location.href = "index.html";
+    return;
+  }
+
+  const { data: profile } = await supabaseClient
+    .from("profiles")
+    .select("*")
+    .eq("id", session.user.id)
+    .single();
+
+  if(!profile){
+    window.location.href = "index.html";
+    return;
+  }
+
+  // ✅ اگر غیرفعال باشد
+  if(profile.is_active === false){
+    await supabaseClient.auth.signOut();
+    alert("حساب شما غیرفعال شده است.");
+    window.location.href = "index.html";
+    return;
+  }
+
+  // ✅ اگر ادمین است اجازه بده
+  if(profile.role === "admin"){
+    return;
+  }
+
+  // ✅ اگر پایه اشتباه باشد
+  if(requiredGrade && profile.grade !== requiredGrade){
+    window.location.href = "grade"+profile.grade+".html";
+    return;
+  }
+
+}
