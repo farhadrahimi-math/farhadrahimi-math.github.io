97fa1cdba56651f39811b998a548a40f9c48c159
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-26 18:28:25 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
+const supabaseUrl = "https://ypjmkigvghybkwyxndcz.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwam1raWd2Z2h5Ymt3eXhuZGN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMTc1NTgsImV4cCI6MjA5OTY5MzU1OH0.lJ5RddKmDdPfLecBsqL9XMGejL9Owbv1ZH2PXSqqdv4";
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
