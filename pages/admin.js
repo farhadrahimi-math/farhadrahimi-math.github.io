9113c7255ccf88765e665f34e8206e9627b77965
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-26 18:12:54 +0330

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,1 +1,9 @@
+export function renderAdmin() {
+    document.getElementById("app").innerHTML = `
+        <h1>پنل مدیریت</h1>
 
+        <button onclick="location.hash='login'">
+            خروج
+        </button>
+    `;
+}
