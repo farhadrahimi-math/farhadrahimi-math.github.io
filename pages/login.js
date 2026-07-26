ba5f6dd4ee94fc0d942dfbeef2d1c5fb266e6477
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-26 18:11:30 +0330

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,1 +1,9 @@
+export function renderLogin() {
+    document.getElementById("app").innerHTML = `
+        <h1>ورود</h1>
 
+        <button onclick="location.hash='dashboard'">
+            ورود
+        </button>
+    `;
+}
