cef615824d8bc4391ae3ad5717c332db0a77fc4a
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-26 18:12:15 +0330

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,1 +1,9 @@
-temp
+export function renderDashboard() {
+    document.getElementById("app").innerHTML = `
+        <h1>داشبورد</h1>
+
+        <button onclick="location.hash='admin'">
+            پنل ادمین
+        </button>
+    `;
+}
