038795c1c2e11ad1924602e061f34a717bc1088b
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-26 18:32:17 +0330

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,56 @@
-export function renderLogin() {
-    document.getElementById("app").innerHTML = `
-        <h1>ورود</h1>
+import { login } from "../auth.js";
 
-        <button onclick="location.hash='dashboard'">
+export function renderLogin() {
+
+    document.getElementById("app").innerHTML = `
+
+    <div class="login">
+
+        <h2>ورود</h2>
+
+        <input
+            id="phone"
+            placeholder="شماره موبایل">
+
+        <input
+            id="password"
+            type="password"
+            placeholder="رمز عبور">
+
+        <button id="loginBtn">
             ورود
         </button>
+
+        <p id="message"></p>
+
+    </div>
+
     `;
+
+    document
+        .getElementById("loginBtn")
+        .addEventListener("click", async () => {
+
+            const phone =
+                document.getElementById("phone").value.trim();
+
+            const password =
+                document.getElementById("password").value;
+
+            const result =
+                await login(phone, password);
+
+            if (result.success) {
+
+                location.hash = "dashboard";
+
+            } else {
+
+                document.getElementById("message").textContent =
+                    result.message;
+
+            }
+
+        });
+
 }
