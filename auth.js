e10777ddac69b6c168e27b84a7aae152413ec4d8
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-26 18:29:26 +0330

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,39 @@
+import { supabase } from "./config.js";
 
+export async function login(phone, password) {
+
+    const email = `${phone}@student.math`;
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if (error) {
+        return {
+            success: false,
+            message: error.message
+        };
+    }
+
+    const { data: profile } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", data.user.id)
+        .single();
+
+    if (!profile.is_active) {
+
+        await supabase.auth.signOut();
+
+        return {
+            success: false,
+            message: "حساب شما غیرفعال است."
+        };
+    }
+
+    return {
+        success: true,
+        user: profile
+    };
+}
