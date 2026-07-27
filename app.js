a6eadf70961b6d157c3cabfa84fa8e677868843b
farhadrahimi-math <farhadrsh76@gmail.com>
2026-07-27 12:33:07 +0330

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { renderLogin } from "./pages/login.js";
 import { renderDashboard } from "./pages/dashboard.js";
 import { renderAdmin } from "./pages/admin.js";
 
-import { getCurrentUser } from "./auth.js";
+import { getCurrentUser, watchProfile } from "./auth.js";
 
 const routes = {
     login: renderLogin,
@@ -10,24 +10,38 @@ const routes = {
     admin: renderAdmin,
 };
 
+let profileChannel = null;
+
 async function router() {
 
     const page = location.hash.replace("#", "") || "login";
 
     const user = await getCurrentUser();
 
-    // اگر کاربر وارد شده باشد و به login برود
+    // اگر کاربر وارد شده باشد، فقط یک بار Realtime را فعال کن
+    if (user && !profileChannel) {
+        profileChannel = watchProfile(user.id);
+    }
+
+    // اگر کاربر وارد شده باشد و صفحه login را باز کند
     if (user && page === "login") {
         location.hash = "dashboard";
         return;
     }
 
-    // اگر وارد نشده باشد و بخواهد صفحات دیگر را ببیند
+    // اگر وارد نشده باشد
     if (!user && page !== "login") {
+
+        if (profileChannel) {
+            await profileChannel.unsubscribe();
+            profileChannel = null;
+        }
+
         location.hash = "login";
         return;
     }
 
+    // نمایش صفحه
     if (routes[page]) {
         routes[page]();
     } else {
