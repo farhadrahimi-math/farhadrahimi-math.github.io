590b066efc1cab6df6e95f667bba29a7094db438
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 10:28:44 +0330

    Update toast.js

--- components/toast.js
+++ components/toast.js
@@ -1,1 +1,31 @@
+export function showToast(message, type = "info") {
 
+    const oldToast = document.querySelector(".toast");
+
+    if (oldToast) {
+        oldToast.remove();
+    }
+
+    const toast = document.createElement("div");
+
+    toast.className = `toast ${type}`;
+
+    toast.textContent = message;
+
+    document.body.appendChild(toast);
+
+    setTimeout(() => {
+        toast.classList.add("show");
+    }, 50);
+
+    setTimeout(() => {
+
+        toast.classList.remove("show");
+
+        setTimeout(() => {
+            toast.remove();
+        }, 300);
+
+    }, 3000);
+
+}
