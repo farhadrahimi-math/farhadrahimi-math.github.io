c8c67d86b08f6755108512671c664ace8d2d0f8c
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 15:40:47 +0330

    Create permissions.js

---
+++ config/permissions.js
@@ -0,0 +1,43 @@
+export const PERMISSIONS = {
+
+    student: [
+
+        "dashboard",
+
+        "chapter",
+
+        "exam",
+
+        "game",
+
+        "report",
+
+        "settings",
+
+        "logout"
+
+    ],
+
+    admin: [
+
+        "dashboard",
+
+        "chapter",
+
+        "exam",
+
+        "game",
+
+        "report",
+
+        "users",
+
+        "contents",
+
+        "settings",
+
+        "logout"
+
+    ]
+
+};
