ffa2fdef1dd331a1a8dff6b70ff4f49e001a61ae
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 10:40:06 +0330

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,1 +1,30 @@
+export function createNavbar(title = "باشگاه نخبگان ریاضی") {
 
+    return `
+
+        <header class="navbar">
+
+            <div class="navbar-title">
+
+                <img
+                    src="assets/images/logo.svg"
+                    class="navbar-logo"
+                    alt="Logo">
+
+                <span>${title}</span>
+
+            </div>
+
+            <button
+                id="menuBtn"
+                class="icon-btn">
+
+                <i data-lucide="menu"></i>
+
+            </button>
+
+        </header>
+
+    `;
+
+}
