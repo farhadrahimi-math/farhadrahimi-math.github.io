fb725ca402b2e0058e7c7aa27d28a2ae444bc452
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 15:36:52 +0330

    Update drawerHeader.js

--- components/drawer/drawerHeader.js
+++ components/drawer/drawerHeader.js
@@ -1,1 +1,28 @@
+export function createDrawerHeader(profile) {
 
+    return `
+
+        <div class="drawer-header">
+
+            <img
+                src="assets/images/logo.svg"
+                class="drawer-avatar"
+                alt="Logo">
+
+            <h3>
+
+                ${profile.name}
+
+            </h3>
+
+            <p>
+
+                پایه ${profile.grade}
+
+            </p>
+
+        </div>
+
+    `;
+
+}
