be4c553bacfb6b14a11179a71d75d588d0a08c40
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 15:43:01 +0330

    Update drawer.js

--- components/drawer/drawer.js
+++ components/drawer/drawer.js
@@ -1,1 +1,28 @@
+import { createDrawerHeader } from "./drawerHeader.js";
+import { createDrawerItem } from "./drawerItem.js";
 
+import { getMenuItems } from "../../services/menuService.js";
+
+export function createDrawer(profile) {
+
+    const items = getMenuItems(profile.role);
+
+    return `
+
+        <div id="drawerOverlay" class="drawer-overlay"></div>
+
+        <aside id="drawer" class="drawer">
+
+            ${createDrawerHeader(profile)}
+
+            <div class="drawer-menu">
+
+                ${items.map(item => createDrawerItem(item)).join("")}
+
+            </div>
+
+        </aside>
+
+    `;
+
+}
