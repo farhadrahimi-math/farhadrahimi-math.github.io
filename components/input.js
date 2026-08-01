2f67eee7e0449f5546a1c0bcd1840f606def4034
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 10:25:40 +0330

    Update input.js

--- components/input.js
+++ components/input.js
@@ -1,1 +1,26 @@
+export function createInput({
+    id = "",
+    label = "",
+    type = "text",
+    placeholder = "",
+    value = ""
+}) {
 
+    return `
+        <div class="input-group">
+
+            <label for="${id}">
+                ${label}
+            </label>
+
+            <input
+                id="${id}"
+                type="${type}"
+                placeholder="${placeholder}"
+                value="${value}"
+                autocomplete="off">
+
+        </div>
+    `;
+
+}
