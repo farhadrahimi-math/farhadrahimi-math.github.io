3303277db2d8038b8596e04e2a5309983d5bab52
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 12:01:06 +0330

    Update chapterCard.js

--- components/dashboard/chapterCard.js
+++ components/dashboard/chapterCard.js
@@ -1,17 +1,41 @@
-export function createChapterCard(index, title) {
+export function createChapterCard({
+    number,
+    title,
+    status = "not-started"
+}) {
+
+    const statusMap = {
+        completed: {
+            text: "تکمیل شده",
+            className: "completed"
+        },
+        progress: {
+            text: "در حال یادگیری",
+            className: "progress"
+        },
+        "not-started": {
+            text: "شروع نشده",
+            className: "not-started"
+        }
+    };
+
+    const current = statusMap[status];
 
     return `
-        <div class="chapter-card" data-chapter="${index + 1}">
+        <div class="chapter-card"
+             data-chapter="${number}">
 
             <div class="chapter-number">
-                ${index + 1}
+                ${number}
             </div>
 
             <div class="chapter-info">
 
                 <h3>${title}</h3>
 
-                <p>ورود به فصل</p>
+                <p class="chapter-status ${current.className}">
+                    ${current.text}
+                </p>
 
             </div>
 
