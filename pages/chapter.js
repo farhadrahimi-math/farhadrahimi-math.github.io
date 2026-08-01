39f4d63fb80b6cf015b09a7b0f1d651d0011bac7
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 13:11:13 +0330

    Update chapter.js

--- pages/chapter.js
+++ pages/chapter.js
@@ -1,1 +1,17 @@
+import { createAppLayout } from "../components/appLayout.js";
 
+export async function renderChapter() {
+
+    document.getElementById("app").innerHTML =
+        createAppLayout(
+
+            `
+                <h2>صفحه فصل</h2>
+                <p>به زودی...</p>
+            `,
+
+            "فصل"
+
+        );
+
+}
