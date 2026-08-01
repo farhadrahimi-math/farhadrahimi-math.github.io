440d81e264dd75df8f2ab1f0f35a2909ecdc893e
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 10:23:39 +0330

    Update button.js

--- components/button.js
+++ components/button.js
@@ -1,1 +1,24 @@
+export function createButton({
 
+    id = "",
+
+    text = "",
+
+    type = "button",
+
+    className = "btn"
+
+}) {
+
+    return `
+        <button
+            id="${id}"
+            type="${type}"
+            class="${className}">
+
+            ${text}
+
+        </button>
+    `;
+
+}
