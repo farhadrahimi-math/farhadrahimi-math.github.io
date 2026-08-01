c2d62d7216c81124d608951aec187e61efce5f22
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 15:36:18 +0330

    Update drawerItem.js

--- components/drawer/drawerItem.js
+++ components/drawer/drawerItem.js
@@ -1,1 +1,32 @@
+export function createDrawerItem({
 
+    id,
+
+    title,
+
+    icon,
+
+    route
+
+}) {
+
+    return `
+
+        <button
+            class="drawer-item"
+            data-route="${route}"
+            id="${id}">
+
+            <i data-lucide="${icon}"></i>
+
+            <span>
+
+                ${title}
+
+            </span>
+
+        </button>
+
+    `;
+
+}
