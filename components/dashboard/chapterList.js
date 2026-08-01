b49deac67a9aee0f175cd5e943c7f4bb158f0545
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 11:57:37 +0330

    Update chapterList.js

--- components/dashboard/chapterList.js
+++ components/dashboard/chapterList.js
@@ -1,1 +1,18 @@
+import { createChapterCard } from "./chapterCard.js";
+import { createSection } from "../section.js";
 
+export function createChapterList(chapters) {
+
+    const cards = chapters
+        .map((chapter, index) =>
+            createChapterCard(index, chapter)
+        )
+        .join("");
+
+    return createSection({
+        title: "فصل‌های ریاضی",
+        icon: "book-open",
+        content: cards
+    });
+
+}
