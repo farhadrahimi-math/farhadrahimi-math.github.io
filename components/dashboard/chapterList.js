c053a24ad3b9216e0d780ebc852d463c5515c332
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 12:01:39 +0330

    Update chapterList.js

--- components/dashboard/chapterList.js
+++ components/dashboard/chapterList.js
@@ -3,11 +3,15 @@ import { createSection } from "../section.js";
 
 export function createChapterList(chapters) {
 
-    const cards = chapters
-        .map((chapter, index) =>
-            createChapterCard(index, chapter)
-        )
-        .join("");
+    const cards = chapters.map((title, index) => {
+
+        return createChapterCard({
+            number: index + 1,
+            title,
+            status: "not-started"
+        });
+
+    }).join("");
 
     return createSection({
         title: "فصل‌های ریاضی",
