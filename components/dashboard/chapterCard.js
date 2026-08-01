bc419a65cdc0ec72777dd424fc8711f2087574fa
farhadrahimi-math <farhadrsh76@gmail.com>
2026-08-01 11:57:04 +0330

    Update chapterCard.js

--- components/dashboard/chapterCard.js
+++ components/dashboard/chapterCard.js
@@ -1,1 +1,21 @@
+export function createChapterCard(index, title) {
 
+    return `
+        <div class="chapter-card" data-chapter="${index + 1}">
+
+            <div class="chapter-number">
+                ${index + 1}
+            </div>
+
+            <div class="chapter-info">
+
+                <h3>${title}</h3>
+
+                <p>ورود به فصل</p>
+
+            </div>
+
+        </div>
+    `;
+
+}
